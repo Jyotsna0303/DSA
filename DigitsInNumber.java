f8816f7ba898d1a58a45e2886bcc2c3121a294e0
Jyotsna Kukreti <30364057+Jyotsna0303@users.noreply.github.com>
2021-05-24 22:51:51 +0530

    Create DigitsInNumber.java

---
+++ DigitsInNumber.java
@@ -0,0 +1,57 @@
+package DSA;
+// Find the number of digits in a number
+
+public class DigitsInNumber {
+    //iterative solution
+    //time complexity= O(n)
+    public int iterativeSolution(int n){
+        int count=0;
+        while(n>0){
+            n=n/10;
+            count++;
+        }
+        return count;
+    }
+  
+    public long iterativeSolution(long n){
+        int count=0;
+        while(n>0){
+            n=n/10;
+            count++;
+        }
+        return count;
+    }
+
+
+    //recursive solution
+    // time complexity O(log n)
+    public int recursiveSol(int n){
+
+        if (n==0){
+            return 0;
+        }
+        return 1+ recursiveSol(n/10);
+    }
+
+
+    //logarithmetic question
+    // if log n= 1.5655.. (log base 10) then n will have 1+1 digits
+    //time complexity=O(1)
+    public int logSolution(int n){
+        if (n>0) {
+            return (int) Math.floor(Math.log10(n) + 1);
+        }
+        return 0;
+    }
+
+    public static void main(String[] args){
+        DigitsInNumber d= new DigitsInNumber();
+        int n=35677;
+        long l= 564890089;
+        System.out.println("Solution with iterative approach:"+d.iterativeSolution(n));
+        System.out.println("Iterative sol for long:"+d.iterativeSolution(l));
+        System.out.println("Solution with recursive approach:"+d.recursiveSol(n));
+        System.out.println("Solution with log approach::"+d.logSolution(n));
+    }
+
+}
