0d065fc5e21967df8bc49b8ea866898f46d1a444
Jyotsna Kukreti <30364057+Jyotsna0303@users.noreply.github.com>
2021-05-25 15:59:53 +0530

    Update CheckPrime.java

--- CheckPrime.java
+++ CheckPrime.java
@@ -12,13 +12,22 @@ public class CheckPrime {
         }
         return true;
     }
-
+     //time complexity= O(sqrt(n))
+    //n=a*b where a<=sqrt(n) and b>=sqrt(n)
+    public boolean sqPrime(int n){
+        for (int i=2;i*i<=n; i++){
+            if (n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args){
         Scanner input= new Scanner(System.in);
         System.out.println("Enter a number:");
         int n= input.nextInt();
         CheckPrime c=new CheckPrime();
-        System.out.println(c.ifPrime(n));
+        System.out.println(c.naivePrime(n));
     }
 
 }
