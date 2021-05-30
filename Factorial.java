d292bdf10f7bee310dc358753216407fa6139f5f
Jyotsna Kukreti <30364057+Jyotsna0303@users.noreply.github.com>
2021-05-30 19:34:40 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,54 @@
+// Initial Template for Java
+//Given a positive integerN. The task is to find factorial of N.
+// Your task is to complete the function factorial() that takes N as parameter and returns factorial of N.
+
+/*Expected Time Complexity : O(N)
+Expected Auxilliary Space : O(1)
+
+Constraints:
+0 <= N <= 18 */
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        
+        //taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        //taking testcases
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            
+            //creating an object of class Factorial
+            Solution obj = new Solution();
+            int N;
+            
+            //taking N
+            N = sc.nextInt();
+            
+            //calling factorial() method 
+            //of class Factorial
+            System.out.println(obj.factorial(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+
+    public long factorial(int N) {
+        // Your code here
+        if (N==0 || N==1){
+          return 1;  
+        }
+        else {
+            return N*factorial(N-1);
+        }
+}
+    
+}
